d73ffe2dcf7922bf7962968f3d48ff267d04e2c8
GabrielaSantos1 <104846859+GabrielaSantos1@users.noreply.github.com>
2023-05-29 23:54:52 -0300

    Update Program.cs

--- TRIANGULO_CLASSE/Program.cs
+++ TRIANGULO_CLASSE/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+
 using System;
 using System.Globalization;
 
@@ -9,21 +10,29 @@ namespace Couse
     {
         public static void Main(string[] args)
         {
+            // cria uma variavel do tipo double que é igual à função "X" do LerMedidasTriangulo
             double[] medidasX = LerMedidasTriangulo("X");
+            // cria uma variavel do tipo double que é igual à função "Y" do LerMedidasTriangulo
             double[] medidasY = LerMedidasTriangulo("Y");
 
+            //cria uma variavel tipo double que é igual à função de CalcularAreaTriangulo com as medidas de X
             double areaX = CalcularAreaTriangulo(medidasX);
+            //cria uma variavel tipo double que é igual à função de CalcularAreaTriangulo com as medidas de Y
             double areaY = CalcularAreaTriangulo(medidasY);
 
+            //console write line fala para o user oq tiver dentro dos (). Fala exatamente o que estiver em "" + os valores das variáveis
             Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
             Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
 
+            //se o valor de areaX for maior que o valor de areaY
             if (areaX > areaY)
             {
+                //fala para o user o texto em ""
                 Console.WriteLine("Maior área: X");
             }
+            //se não
             else
-            {
+            {   // fala para o user ""
                 Console.WriteLine("Maior área: Y");
             }
 
